55f1344f1b87eb28b038ba88da2cc87603261b3f
sdh0192 <sdh0192@live.com>
2019-01-20 18:48:10 -0500

    adding some updates

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
+// clientID 930343641316-pb71lju89gtnac9ls95o5t27tsefgc5t.apps.googleusercontent.com
+// client secret Bu8IPjQtnZrrYp1SW8c69Dga
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
