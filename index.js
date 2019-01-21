6ebc838a3150687b458bb3bda3124dc157709903
sdh0192 <sdh0192@live.com>
2019-01-20 20:11:40 -0500

    google oath updates

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 const app = express();
 
-// clientID 930343641316-pb71lju89gtnac9ls95o5t27tsefgc5t.apps.googleusercontent.com
-// client secret Bu8IPjQtnZrrYp1SW8c69Dga
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
-const PORT = process.env.PORT || 5000
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-
