8f3e56583dda29b6324ba98bc0aa707f48753fe0
Scott Hastings <sdh0192@live.com>
2019-02-11 21:45:51 -0500

    still not sure of issue with google+

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,6 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
-      userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
